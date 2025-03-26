1082ac83744c19781cab7621f8c2ec3d1afd0137
Suvo Ghosh <suvosh663@gmail.com>
2025-03-26 20:01:45 +0530

    Add second anagram checking method using HashMap

--- Anagram Check/anagramCheck.java
+++ Anagram Check/anagramCheck.java
@@ -1,8 +1,9 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class anagramCheck {
 
-    public static boolean isAnagram(String str1, String str2) {
+    public static boolean isAnagram_1(String str1, String str2) {
         if (str1.length() != str2.length())
             return false;
         char[] arr1 = str1.toCharArray();
@@ -12,9 +13,27 @@ public class anagramCheck {
         return Arrays.equals(arr1, arr2);
     }
 
+    public static boolean isAnagram_2(String str1, String str2) {
+        if (str1.length() != str2.length())
+            return false;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str1.length(); i++) {
+            map.put(str1.charAt(i), map.getOrDefault(str1.charAt(i), 0) + 1);
+            map.put(str2.charAt(i), map.getOrDefault(str2.charAt(i), 0) - 1);
+        }
+
+        for (int i : map.values()) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         String str1 = "iltsen";
         String str2 = "Silent";
-        System.out.println(isAnagram(str1.toLowerCase(), str2.toLowerCase()));
+        System.out.println(isAnagram_1(str1.toLowerCase(), str2.toLowerCase()));
+        System.out.println(isAnagram_2(str1.toLowerCase(), str2.toLowerCase()));
     }
 }
